71aa08e616128ac3b622126368cbce810b6e4510
pncalbl <58671677+pncalbl@users.noreply.github.com>
2022-05-31 01:33:06 +0800

    first commit

---
+++ src/main/java/com/pncalbl/pncamusic/PncaMusicApplication.java
@@ -0,0 +1,13 @@
+package com.pncalbl.pncamusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PncaMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PncaMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pncalbl/pncamusic/PncaMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pncalbl.pncamusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PncaMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
