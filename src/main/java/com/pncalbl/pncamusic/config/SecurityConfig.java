b609b0dcc6249c2344820ced4c952fb1554b553d
pncalbl <58671677+pncalbl@users.noreply.github.com>
2022-06-09 22:06:05 +0800

    Update SecurityConfig.java

--- src/main/java/com/pncalbl/pncamusic/config/SecurityConfig.java
+++ src/main/java/com/pncalbl/pncamusic/config/SecurityConfig.java
@@ -27,7 +27,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public static final long EXPIRATION_TIME = 864000000; // 10 days
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
-	public static final String SIGN_UP_URL = "/users/";   // 后续需要关闭
 
 	UserService userService;
 	RestAuthenticationEntryPoint restAuthenticationEntryPoint;
